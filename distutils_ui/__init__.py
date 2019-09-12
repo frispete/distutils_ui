3656b635e2b2decbaa09da799a293a6de7b91520
Hans-Peter Jansen <hp@urpla.net>
2019-09-12 16:11:55 +0200

    version 0.1.3

--- distutils_ui/__init__.py
+++ distutils_ui/__init__.py
@@ -4,4 +4,4 @@ Distutils/Setuptools build extension for PyQt{4,5} applications
     * build_ui: Build UI specific elements in tree, controlled by setup.cfg.
 
 """
-__version__ = '0.1.2'
+__version__ = '0.1.3'
